d624e6ea1ac01c9176f64db768891c571894a166
hirorock1103 <mdiz1103@gmail.com>
2020-06-24 17:20:09 +0900

    リンク修正など

--- public/select.php
+++ public/select.php
@@ -15,35 +15,36 @@ $entry_mode = (find_record_by($dbh, 'PaymentInfo', 'seq', 'gmo_id', $ses['cs_id'
 		<div class="block-gosmania2">
 			<div id="aplly_kind00" class="app btn sp_none flex-buttons">
 					<?php $class = ($entry_mode == false) ? "disable" : "";  ?>
-					<a class="btn-sub btn-select <?php echo $class;  ?>" href="/entry.php">
+					<a class="btn-sub btn-select <?php echo $class;  ?>" href="./entry.php">
 						<i class="fas fa-edit" style="position: absolute; top: 12px; left: 40px;"></i>クレジットカード新規登録はこちら
 					</a>
 
 					<?php $class = ($entry_mode == true) ? "disable" : "";  ?>
-					<a class="btn-sub btn-select <?php echo $class;  ?>" href="/credit_edit.php" class="btn-sub btn-select">
+					<a class="btn-sub btn-select <?php echo $class;  ?>" href="./credit_edit.php" class="btn-sub btn-select">
 						<i class="fas fa-sync-alt" style="position: absolute; top: 12px; left: 40px;"></i>クレジットカード更新はこちら
 					</a>
-					<a class="btn-sub btn-select" href="/customer_info_edit.php"  class="btn-sub btn-select">
+					<a class="btn-sub btn-select" href="./customer_info_edit.php"  class="btn-sub btn-select">
 						<i class="far fa-envelope" style="position: absolute; top: 12px; left: 40px;"></i>お客様情報の登録・更新はこちら
 					</a>
 			</div>
 
 			<div id="aplly_kind00" class="app btn pc_none flex-buttons">
 					<?php $class = ($entry_mode == false) ? "disable" : "";  ?>
-					<a name="action" value="send" href="/entry.php" class="btn-sub btn-select <?php echo $class;  ?>"  style="width:90%;">
+					<a name="action" value="send" href="./entry.php" class="btn-sub btn-select <?php echo $class;  ?>"  style="width:90%;">
 						<i class="fas fa-edit" style="position: absolute; top: 12px; left: 40px;"></i>クレジットカード<br>新規登録はこちら
 					</a>
 
 					<?php $class = ($entry_mode == true) ? "disable" : "";  ?>
-					<a href="/credit_edit.php" class="btn-sub btn-select <?php echo $class;  ?>" style="width:90%;">
+					<a href="./credit_edit.php" class="btn-sub btn-select <?php echo $class;  ?>" style="width:90%;">
 						<i class="fas fa-sync-alt" style="position: absolute; top: 12px; left: 40px;"></i>クレジットカード<br>更新はこちら
 					</a>
-					<a href="/customer_info_edit.php" class="btn-sub btn-select" style="width:90%;">
+					<a href="./customer_info_edit.php" class="btn-sub btn-select" style="width:90%;">
 						<i class="far fa-envelope" style="position: absolute; top: 12px; left: 40px;"></i>お客様情報の<br>登録・更新はこちら
 					</a>
 			</div>
 
 		</div>
+		<div class="block-gosmania2--comment"><a class="link-type-1" href="">特定商取引法に関する表記</a></div>
 
 	</section>
 
