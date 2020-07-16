75ee3e59ff5dd6e59aabac05b1b4da0da1c4ad99
hirorock1103 <mdiz1103@gmail.com>
2020-07-16 16:47:15 +0900

    test

--- public/admin/cron_functions/send_mail_functions.php
+++ public/admin/cron_functions/send_mail_functions.php
@@ -233,6 +233,7 @@ function executeSendMailtoTarget($dbh, $Sm_Type, $Customers) {
 			// 顧客情報に基づいてメール本文を生成
 			$mailContent = generateMailContent($sendMail, $customer);
 
+			var_dump($mailContent);
 			// メール送信実行
 			$sendCount += mb_send_mail(
 				$customer['Ci_MailAddress'],
