f45d0d4768e148c7d9b55881e4ddb2e312497a28
hirorock1103 <mdiz1103@gmail.com>
2020-07-30 06:45:23 +0900

    ip

--- system/admin/settings.php
+++ system/admin/settings.php
@@ -5,12 +5,6 @@ define("SESSION_BASE_NAME", "gosmania");
 session_name(constant("SESSION_BASE_NAME"));
 session_start();
 
-//ip制限
-$allow_ip = array("119.243.84.174");
-if( !in_array($_SERVER["REMOTE_ADDR"], $allow_ip)  ){
-	header("Location: logout.php?logout");
-	exit;
-}
 
 // アプリケーション設定
 include_once dirname(__FILE__) . "/../../common/config.php";
@@ -18,6 +12,15 @@ include_once dirname(__FILE__) . "/../../common/config.php";
 $file = basename($_SERVER['SCRIPT_NAME'], ".php");
 if ($file != "login" && $file != "logout") { // ログイン／ログアウト画面以外で
 	if(!isset($_SESSION[SESSION_BASE_NAME]['login_info'])) { // SESSION情報なければ、もしくはリリース作業中のアクセスだったら
+		header("Location: logout.php?logout");
+		exit;
+	}
+}
+
+//ip制限
+$allow_ip = array("119.243.84.174");
+if( !in_array($_SERVER["REMOTE_ADDR"], $allow_ip)  ){
+	if ($file != "login" && $file != "logout") { // ログイン／ログアウト画面以外で
 		header("Location: logout.php?logout");
 		exit;
 	}
