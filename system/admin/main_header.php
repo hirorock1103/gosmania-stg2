39ad0103bf88c0681e886adbde314ffcfdf6da1b
hirorock1103 <mdiz1103@gmail.com>
2020-08-19 11:34:20 +0900

    調整

--- system/admin/main_header.php
+++ system/admin/main_header.php
@@ -23,7 +23,7 @@
 				</a>
 			</div>
 			<div>
-				<a class="header_text" href="./login.php?logout"><i class="fas fa-sign-out-alt"></i>
+				<a class="header_text" href="./logout.php"><i class="fas fa-sign-out-alt"></i>
 				</a>
 			</div>
 		</div>
